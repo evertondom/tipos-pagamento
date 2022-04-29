c4db8af90a43d43c5f692ad2513224aeec8fa652
evertondom <jony.everton-@hotmail.com>
2022-04-29 13:29:05 -0300

    Tipos de Pagamentos

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Aula4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblEscolhido = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.lblValorTotal = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(30, 124);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(172, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Formas de Pagamento";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(227, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(195, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Método de pagamento foi:";
+            // 
+            // lblEscolhido
+            // 
+            this.lblEscolhido.AutoSize = true;
+            this.lblEscolhido.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEscolhido.Location = new System.Drawing.Point(428, 124);
+            this.lblEscolhido.Name = "lblEscolhido";
+            this.lblEscolhido.Size = new System.Drawing.Size(0, 18);
+            this.lblEscolhido.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Valor do Pagamento:";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(165, 32);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(131, 20);
+            this.txtValor.TabIndex = 5;
+            // 
+            // lblValorTotal
+            // 
+            this.lblValorTotal.AutoSize = true;
+            this.lblValorTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValorTotal.Location = new System.Drawing.Point(270, 254);
+            this.lblValorTotal.Name = "lblValorTotal";
+            this.lblValorTotal.Size = new System.Drawing.Size(0, 20);
+            this.lblValorTotal.TabIndex = 7;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(30, 151);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(73, 21);
+            this.comboBox2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.lblValorTotal);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblEscolhido);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblEscolhido;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label lblValorTotal;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Aula4
+{
+    public partial class Form1 : Form
+    {
+        decimal valor = 0.00m;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //criar uma arraylist
+            ArrayList formaPagto = new ArrayList();
+            formaPagto.Add(new FormaDePgto(0, "Selecione Opção"));
+            formaPagto.Add(new FormaDePgto(1, "Dinheiro"));
+            formaPagto.Add(new FormaDePgto(2, "Cartão"));
+            formaPagto.Add(new FormaDePgto(3, "Boleto"));
+            formaPagto.Add(new FormaDePgto(4, "Pix"));
+
+            //vincular ao combobox1
+            comboBox1.DataSource = formaPagto;
+            comboBox1.DisplayMember = "Descricao";
+            comboBox1.ValueMember = "ID";
+           
+        }
+
+        public class FormaDePgto
+        {
+            public int ID { get; set; }
+            public string Descricao { get; set; }
+
+            public FormaDePgto(int iD, string descricao)
+            {
+                ID = iD;
+                Descricao = descricao;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            valor = decimal.Parse(txtValor.Text);
+            int escolha = comboBox1.SelectedIndex;
+            switch (escolha)
+            {
+                case 0:
+                    lblEscolhido.Text = "Escolha Opção";
+                    break;
+                case 1:
+                    lblEscolhido.Text = "Dinheiro";
+                    lblValorTotal.Text = $"Valor a ser pago: {valor:F2} ";
+                    break;
+                case 2:
+                    lblEscolhido.Text = "Cartão";
+                    ArrayList qtdPagto = new ArrayList();
+                    for (int i = 0; i < 10; i++)
+                    {
+                        qtdPagto.Add(i+1);
+                    }
+                    comboBox2.DataSource = qtdPagto;
+
+                    lblValorTotal.Text = $"{comboBox2.SelectedIndex}x de {valor / comboBox2.SelectedIndex + 1}";
+                    
+                    break;
+                case 3:
+                    lblEscolhido.Text = "Boleto";
+                    lblValorTotal.Text = $"Valor a ser pago: {valor:F2} ";
+                    break;
+                case 4:
+                    lblEscolhido.Text = "Pix";
+                    lblValorTotal.Text = $"Valor a ser pago: {valor:F2} ";
+                    break;
+                default:
+                    lblEscolhido.Text = ""; 
+                    break;
+            }
+
+            
+        }
+    }
+}
